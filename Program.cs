4417c10aca4531534c334214df73c6cf8b7fd484
ctschnurr <ctschnurr@hotmail.com>
2022-09-14 13:49:42 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Please press any key to exit!");
+            Console.ReadKey();
+
+        }
+    }
+}
